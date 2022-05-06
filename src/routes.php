99d7c4c592f32c85705a3acf1be9378c5c8a0dbe
Hubert Krzysztofiak <62691459+HerbertIV@users.noreply.github.com>
2022-05-06 12:01:26 +0200

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 use EscolaLms\AssignWithoutAccount\Http\Controllers\UserSubmissionAdminController;
-use EscolaLms\Core\Http\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'api'], function () {
-    Route::group(['prefix' => 'admin/user-submissions', 'middleware' => Route::apply(['auth:api'])], function () {
+    Route::group(['prefix' => 'admin/user-submissions', 'middleware' => ['auth:api']], function () {
         Route::get('/', [UserSubmissionAdminController::class, 'index']);
         Route::post('/', [UserSubmissionAdminController::class, 'create']);
         Route::put('/{id}', [UserSubmissionAdminController::class, 'update']);
