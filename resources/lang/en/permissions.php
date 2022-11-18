78c8be870eedd958bad7db4958fcbd7639926aef
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 09:22:00 +0100

    Add translations

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,10 @@
+<?php
+
+use EscolaLms\AssignWithoutAccount\Enums\AssignWithoutAccountPermissionEnum;
+
+return [
+    AssignWithoutAccountPermissionEnum::USER_SUBMISSION_LIST => 'Submission list',
+    AssignWithoutAccountPermissionEnum::USER_SUBMISSION_CREATE => 'Create submission',
+    AssignWithoutAccountPermissionEnum::USER_SUBMISSION_UPDATE => 'Update submission',
+    AssignWithoutAccountPermissionEnum::USER_SUBMISSION_DELETE => 'Delete submission',
+];
